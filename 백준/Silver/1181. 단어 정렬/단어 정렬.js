8af8afb6e746f9e71b377d359f04e0ec9f2bc0ba
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-10-02 22:07:14 +0900

    [Silver V] Title: 단어 정렬, Time: 4612 ms, Memory: 19200 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -4,10 +4,8 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n").slice(1);
 
 const uniqueArr = input.filter((v, idx) => input.indexOf(v) === idx);
 
-const sortedArr = uniqueArr
-  .sort((a, b) =>
-    a.length == b.length ? a.localeCompare(b) : a.length - b.length
-  )
-  .join("\n");
+const sortedArr = uniqueArr.sort((a, b) =>
+  a.length == b.length ? a.localeCompare(b) : a.length - b.length
+);
 
-console.log(sortedArr);
+console.log(sortedArr.join("\n"));
