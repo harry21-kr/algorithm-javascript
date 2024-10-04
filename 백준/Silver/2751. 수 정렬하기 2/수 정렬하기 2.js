b0afbc9ba33456b94cc8b5dc1884ca14e8dc6691
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2024-10-04 16:56:13 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1976 ms, Memory: 176412 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .slice(1)
+  .sort((a, b) => a - b);
+
+console.log(input.join("\n"));
