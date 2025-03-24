21a7373aa67801210c15d7fcf6cfcf1eb14e190b
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2025-03-24 19:48:51 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 180 ms, Memory: 11908 KB -BaekjoonHub

---
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().split(" ");
+
+let arr = [];
+const answerArr = [];
+
+let pointer = +input[1] - 1;
+
+for (let i = 1; i <= +input[0]; i++) {
+  arr.push(i);
+}
+
+while (1) {
+  if (!arr.length) break;
+
+  if (pointer >= arr.length) {
+    pointer -= arr.length;
+  } else if (pointer < arr.length) {
+    answerArr.push(arr[pointer]);
+    arr = arr.filter((v) => v !== arr[pointer]);
+    pointer += +input[1] - 1;
+  }
+}
+
+console.log(`<${answerArr.join(", ")}>`);
