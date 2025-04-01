d69355edaa673bca066bfa6ee30829071b5b3701
Park Won Bin <119783002+harry21-kr@users.noreply.github.com>
2025-04-01 21:21:55 +0900

    [Bronze II] Title: 수 정렬하기, Time: 100 ms, Memory: 9696 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split("\n")
+  .map(Number);
+
+input.shift();
+
+console.log(input.sort((a, b) => a - b).join("\n"));
